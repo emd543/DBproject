a5a993f212e2eefb5d5be0df4ab9000149545ffd
MadeUpName <emd543@nyu.edu>
2019-07-23 16:54:05 +0200

    DB class #5

--- Creating_textbook_DB.sql
+++ Creating_textbook_DB.sql
@@ -4,69 +4,75 @@
 CREATE DATABASE textbook;
 
 --department
-create table department(
+CREATE TABLE department(
   dept_name VARCHAR(20),
   building VARCHAR(15),
   budget NUMERIC(12,2),
+  CHECK (budget > 0),
   PRIMARY KEY(dept_name)
 );
-insert into department VALUES('Comp. Sci.', 'Taylor', 100000);
-insert into department VALUES('Biology', 'Watson', 90000);
-insert into department VALUES('Elec. Eng.', 'Taylor', 85000);
-insert into department VALUES('Music', 'Packard', 80000);
-insert into department VALUES('Finance', 'Painter', 120000);
-insert into department VALUES('History', 'Painter', 50000);
-insert into department VALUES('Physics', 'Watson', 70000);
+--ALTER TABLE department ADD CHECK (budget > 0);
+INSERT INTO department VALUES('Comp. Sci.', 'Taylor', 100000);
+INSERT INTO department VALUES('Biology', 'Watson', 90000);
+INSERT INTO department VALUES('Elec. Eng.', 'Taylor', 85000);
+INSERT INTO department VALUES('Music', 'Packard', 80000);
+INSERT INTO department VALUES('Finance', 'Painter', 120000);
+INSERT INTO department VALUES('History', 'Painter', 50000);
+INSERT INTO department VALUES('Physics', 'Watson', 70000);
 
 --instructor
-create table instructor(
+CREATE TABLE instructor(
   ID VARCHAR(5),
   name VARCHAR(20) NOT NULL,
   dept_name VARCHAR(20),
   salary NUMERIC(8,2),
+  CHECK (salary > 29000),
   PRIMARY KEY(ID),
   FOREIGN KEY(dept_name)
   REFERENCES department(dept_name) ON DELETE SET NULL
 );
-insert into instructor VALUES(22222, 'Einstein', 'Physics', 95000);
-insert into instructor VALUES(12121, 'Wu', 'Finance', 90000);
-insert into instructor VALUES(32343, 'El Said', 'History', 60000);
-insert into instructor VALUES(45565, 'Katz', 'Comp. Sci.', 75000);
-insert into instructor VALUES(98345, 'Kim', 'Elec. Eng.', 80000);
-insert into instructor VALUES(76766, 'Crick', 'Biology', 72000);
-insert into instructor VALUES(10101, 'Srinivasan', 'Comp. Sci.', 65000);
-insert into instructor VALUES(58583, 'Califieri', 'History', 62000);
-insert into instructor VALUES(83821, 'Brandt', 'Comp. Sci.', 92000);
-insert into instructor VALUES(15151, 'Mozart', 'Music', 40000);
-insert into instructor VALUES(33456, 'Gold', 'Physics', 87000);
-insert into instructor VALUES(76543, 'Singh', 'Finance', 80000);
+--ALTER TABLE instructor ADD CHECK (salary > 29000);
+INSERT INTO instructor VALUES(22222, 'Einstein', 'Physics', 95000);
+INSERT INTO instructor VALUES(12121, 'Wu', 'Finance', 90000);
+INSERT INTO instructor VALUES(32343, 'El Said', 'History', 60000);
+INSERT INTO instructor VALUES(45565, 'Katz', 'Comp. Sci.', 75000);
+INSERT INTO instructor VALUES(98345, 'Kim', 'Elec. Eng.', 80000);
+INSERT INTO instructor VALUES(76766, 'Crick', 'Biology', 72000);
+INSERT INTO instructor VALUES(10101, 'Srinivasan', 'Comp. Sci.', 65000);
+INSERT INTO instructor VALUES(58583, 'Califieri', 'History', 62000);
+INSERT INTO instructor VALUES(83821, 'Brandt', 'Comp. Sci.', 92000);
+INSERT INTO instructor VALUES(15151, 'Mozart', 'Music', 40000);
+INSERT INTO instructor VALUES(33456, 'Gold', 'Physics', 87000);
+INSERT INTO instructor VALUES(76543, 'Singh', 'Finance', 80000);
 
 --course
-create table course(
+CREATE TABLE course(
   course_id VARCHAR(7),
   title VARCHAR(50),
   dept_name VARCHAR(20),
   credits NUMERIC(2,0),
+  CHECK (credits > 0),
   PRIMARY KEY(course_id),
   FOREIGN KEY(dept_name)
   REFERENCES department(dept_name) ON DELETE CASCADE
 );
-insert into course VALUES('BIO-101', 'Intro. to Biology', 'Biology', 4);
-insert into course VALUES('BIO-301', 'Genetics', 'Biology', 4);
-insert into course VALUES('BIO-399', 'Computational Biology', 'Biology', 3);
-insert into course VALUES('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4);
-insert into course VALUES('CS-190', 'Game Design', 'Comp. Sci.', 4);
-insert into course VALUES('CS-315', 'Robotics', 'Comp. Sci.', 3);
-insert into course VALUES('CS-319', 'Image Processing', 'Comp. Sci.', 3);
-insert into course VALUES('CS-347', 'Database System Concepts', 'Comp. Sci.', 3);
-insert into course VALUES('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3);
-insert into course VALUES('FIN-201', 'Investment Banking', 'Finance', 3);
-insert into course VALUES('HIS-351', 'World History', 'History', 3);
-insert into course VALUES('MU-199', 'Music Video Production', 'Music', 3);
-insert into course VALUES('PHY-101', 'Physical Principles', 'Physics', 4);
+--ALTER TABLE course ADD CHECK (credits > 0);
+INSERT INTO course VALUES('BIO-101', 'Intro. to Biology', 'Biology', 4);
+INSERT INTO course VALUES('BIO-301', 'Genetics', 'Biology', 4);
+INSERT INTO course VALUES('BIO-399', 'Computational Biology', 'Biology', 3);
+INSERT INTO course VALUES('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4);
+INSERT INTO course VALUES('CS-190', 'Game Design', 'Comp. Sci.', 4);
+INSERT INTO course VALUES('CS-315', 'Robotics', 'Comp. Sci.', 3);
+INSERT INTO course VALUES('CS-319', 'Image Processing', 'Comp. Sci.', 3);
+INSERT INTO course VALUES('CS-347', 'Database System Concepts', 'Comp. Sci.', 3);
+INSERT INTO course VALUES('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3);
+INSERT INTO course VALUES('FIN-201', 'Investment Banking', 'Finance', 3);
+INSERT INTO course VALUES('HIS-351', 'World History', 'History', 3);
+INSERT INTO course VALUES('MU-199', 'Music Video Production', 'Music', 3);
+INSERT INTO course VALUES('PHY-101', 'Physical Principles', 'Physics', 4);
 
 --prereq
-create table prereq(
+CREATE TABLE prereq(
   course_id VARCHAR(7),
   prereq_id VARCHAR(7),
   PRIMARY KEY (course_id, prereq_id),
@@ -75,43 +81,47 @@ create table prereq(
   FOREIGN KEY(prereq_id)
   REFERENCES course(course_id) ON DELETE CASCADE
 );
-insert into prereq VALUES('BIO-301', 'BIO-101');
-insert into prereq VALUES('BIO-399', 'BIO-101');
-insert into prereq VALUES('CS-190', 'CS-101');
-insert into prereq VALUES('CS-315', 'CS-101');
-insert into prereq VALUES('CS-319', 'CS-101');
-insert into prereq VALUES('CS-347', 'CS-101');
-insert into prereq VALUES('EE-181', 'PHY-101');
+INSERT INTO prereq VALUES('BIO-301', 'BIO-101');
+INSERT INTO prereq VALUES('BIO-399', 'BIO-101');
+INSERT INTO prereq VALUES('CS-190', 'CS-101');
+INSERT INTO prereq VALUES('CS-315', 'CS-101');
+INSERT INTO prereq VALUES('CS-319', 'CS-101');
+INSERT INTO prereq VALUES('CS-347', 'CS-101');
+INSERT INTO prereq VALUES('EE-181', 'PHY-101');
 
 --section
-create table section(
+CREATE TABLE section(
   course_id VARCHAR(8),
   sec_id VARCHAR(8),
   semester  VARCHAR(6),
+  CHECK (semester IN ('Fall', 'Winter', 'Spring', 'Summer')),
   year NUMERIC(4,0),
+  CHECK (year > 1759 AND year <2100),
   building VARCHAR(15),
   room_number VARCHAR(7),
   time_slot_id VARCHAR(4),
   PRIMARY KEY(course_id, sec_id, semester, year),
   FOREIGN KEY (course_id) REFERENCES course(course_id),
-  CHECK (semester IN ('Fall', 'Winter', 'Spring', 'Summer'))
+  --FOREIGN KEY (building) REFERENCES classroom(building)
+  --Not implemented yet because classroom relation isn't populated yet
 );
+--ALTER TABLE section ADD CHECK (year > 1759 AND year <2100);
 --ALTER TABLE section ADD CHECK (semester IN ('Fall', 'Winter', 'Spring', 'Summer'));
-insert into section VALUES('BIO-101', '1', 'Summer', 2009, 'Painter', '514', 'B');
-insert into section VALUES('BIO-301', '1', 'Summer', 2010, 'Painter', '514', 'A');
-insert into section VALUES('CS-101', '1', 'Fall', 2009, 'Packard', '101', 'H');
-insert into section VALUES('CS-101', '1', 'Spring', 2010, 'Packard', '101', 'F');
-insert into section VALUES('CS-190', '1', 'Spring', 2009, 'Taylor', '3128', 'E');
-insert into section VALUES('CS-190', '2', 'Spring', 2009, 'Taylor', '3128', 'A');
-insert into section VALUES('CS-315', '1', 'Spring', 2010, 'Watson', '120', 'D');
-insert into section VALUES('CS-319', '1', 'Spring', 2010, 'Watson', '100', 'B');
-insert into section VALUES('CS-319', '2', 'Spring', 2010, 'Taylor', '3128', 'C');
-insert into section VALUES('CS-347', '1', 'Fall', 2009, 'Taylor', '3128', 'A');
-insert into section VALUES('EE-181', '1', 'Spring', 2009, 'Taylor', '3128', 'C');
-insert into section VALUES('FIN-201', '1', 'Spring', 2010, 'Packard', '101', 'B');
-insert into section VALUES('HIS-351', '1', 'Spring', 2010, 'Painter', '514', 'C');
-insert into section VALUES('MU-199', '1', 'Spring', 2010, 'Packard', '101', 'D');
-insert into section VALUES('PHY-101', '1', 'Fall', 2009, 'Watson', '100', 'A');
+INSERT INTO section VALUES('BIO-101', '1', 'Summer', 2009, 'Painter', '514', 'B');
+INSERT INTO section VALUES('BIO-301', '1', 'Summer', 2010, 'Painter', '514', 'A');
+INSERT INTO section VALUES('CS-101', '1', 'Fall', 2009, 'Packard', '101', 'H');
+INSERT INTO section VALUES('CS-101', '1', 'Spring', 2010, 'Packard', '101', 'F');
+INSERT INTO section VALUES('CS-190', '1', 'Spring', 2009, 'Taylor', '3128', 'E');
+INSERT INTO section VALUES('CS-190', '2', 'Spring', 2009, 'Taylor', '3128', 'A');
+INSERT INTO section VALUES('CS-315', '1', 'Spring', 2010, 'Watson', '120', 'D');
+INSERT INTO section VALUES('CS-319', '1', 'Spring', 2010, 'Watson', '100', 'B');
+INSERT INTO section VALUES('CS-319', '2', 'Spring', 2010, 'Taylor', '3128', 'C');
+INSERT INTO section VALUES('CS-347', '1', 'Fall', 2009, 'Taylor', '3128', 'A');
+INSERT INTO section VALUES('EE-181', '1', 'Spring', 2009, 'Taylor', '3128', 'C');
+INSERT INTO section VALUES('FIN-201', '1', 'Spring', 2010, 'Packard', '101', 'B');
+INSERT INTO section VALUES('HIS-351', '1', 'Spring', 2010, 'Painter', '514', 'C');
+INSERT INTO section VALUES('MU-199', '1', 'Spring', 2010, 'Packard', '101', 'D');
+INSERT INTO section VALUES('PHY-101', '1', 'Fall', 2009, 'Watson', '100', 'A');
 
 --teaches
 create table teaches(
@@ -200,6 +210,14 @@ INSERT INTO takes values('98765', 'CS-101', '1', 'Fall', 2009, 'C-');
 INSERT INTO takes values('98765', 'CS-315', '1', 'Spring', 2010, 'B');
 INSERT INTO takes values('98988', 'BIO-101', '1', 'Summer', 2009, 'A');
 INSERT INTO takes values('98988', 'BIO-301', '1', 'Summer', 2010, NULL);
+
+--classroom
+CREATE TABLE classroom(
+  building VARCHAR(15),
+  room_number VARCHAR(7),
+  capacity NUMERIC(4,0),
+  PRIMARY KEY (building, room_number)
+);
 
 --Views
 CREATE VIEW physics_fall_2009 AS
