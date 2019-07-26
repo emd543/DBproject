13bd2e943f9d328494dc40bf0e09a87285b2ae38
MadeUpName <emd543@nyu.edu>
2019-07-26 14:49:01 +0200

    PHP #7

--- main.php
+++ main.php
@@ -1,11 +1,30 @@
-<!DOCTYPE <php>
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <title>Title</title>
+
   </head>
   <body>
-    <h1>This is a test</h1>
-    <p>This is a paragraph</p>
+
+    <form action="site.php" method="get">
+      Noun: <input type="text" name="noun"><br>
+      Person in room: <input type="text" name="personInRoom"><br>
+      Verb: <input type="text" name="verb"><br>
+      Part of the body (plural): <input type="text" name="bodyPart"><br>
+      Adjective: <input type="text" name="adjective"><br>
+
+      <input type="submit">
+    </form>
+
+    <?php
+    $noun = $_GET["noun"];
+    $personInRoom = $_GET["personInRoom"];
+    $verb = $_GET["verb"];
+    $bodyPart = $_GET["bodyPart"];
+    $adjective = $_GET["adjective"];
+    echo "It was Thanksgiving, and the scent of succulent roast $noun wafted through my house. '$personInRoom, it's time to $verb!' my mother called. I couldn't wait to get my $bodyPart on that $adjective Thanksgiving meal.";
+     ?>
+
   </body>
 </html>
