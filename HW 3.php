0dc897ff1291ffaf3cd8275dc069694fa3d57736
MadeUpName <emd543@nyu.edu>
2019-07-29 15:12:59 +0200

    DB class #12

--- HW 3.php
+++ HW 3.php
@@ -44,7 +44,7 @@
     }*/
 
     // Insert data into table
-    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+/*    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
     VALUES ('John', 'Doe', 'john@example.com');";
     $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
     VALUES ('Mary', 'Moe', 'mary@example.com');";
@@ -55,6 +55,19 @@
       echo "New records created successfully";
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
+    }*/
+
+    // Select records from relation
+    $sql = "SELECT id, firstname, lastname FROM MyGuests WHERE firstname='John'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+    } else {
+        echo "0 results";
     }
 
     $conn->close();
